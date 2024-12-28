18ec464c1d1755a1926547ef7c50228525c23823
Rushikesh Sawarkar <167119542+Developer-rishii@users.noreply.github.com>
2024-12-28 17:07:01 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -57,5 +57,5 @@ app.post("/submit", async (req, res) => {
 });
 
 // Start the server
-const PORT = 3000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
